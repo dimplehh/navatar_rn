a149d6e386b601f1e51f2b08f6439fa6587dcf74
dimplehh <dimplehh@naver.com>
2022-04-17 17:18:48 +0900

    상단 메뉴 만듦(아직 완벽 x)

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Image, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar ,ActivityIndicator} from 'react-native';
-import {apoc} from '../../../clothes_information.json';
+import React, {useState} from 'react';
+import { TouchableOpacity, Image, ScrollView, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar ,ActivityIndicator} from 'react-native';
+import {apoc, romanticcrown, kirsh, luvistrue, lee} from '../../../clothes_information.json';
 
 const GridView = ({ name, image, price}) => (
     <View style={styles.gridbox}>
@@ -11,13 +11,25 @@ const GridView = ({ name, image, price}) => (
   );
 
 const Index = () => {
+    const [dataName, setDataName] = useState(apoc["001"]);
+
+    const onPress1 = () => setDataName(apoc["001"]);
+    const onPress2 = () => setDataName(apoc["003"]);
+    const onPress3 = () => setDataName(luvistrue["022"]);//이 로직 지금 겉보기에만 그럴듯하고 개엉망임 flatList같은거 사용해서 버튼도 때에 따라 같은 onPress더라도 다른 결과 추출하도록 해야
       return (
         <SafeAreaView style={styles.container}>
+            <ScrollView horizontal={true}>
+                <TouchableOpacity style={styles.touch} onPress={onPress1}><Text style={{textAlign: "center"}}>Top</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.touch} onPress={onPress2}><Text style={{textAlign: "center"}}>Pants</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.touch} onPress={onPress3}><Text style={{textAlign: "center"}}>Skirt</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.touch}><Text style={{textAlign: "center"}}>My button</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.touch}><Text style={{textAlign: "center"}}>My button</Text></TouchableOpacity>
+            </ScrollView>
           <FlatList numColumns={2}
-            data={apoc["001"]}
-            renderItem={({item})=><GridView name={item.name} image={item.main_image} price={item.price}/>}
+            data={dataName}
+            renderItem={({item})=><GridView name={item.name}
+            image={item.main_image} price={item.price}/>}
             keyExtractor={item => item.name}
-            key={item=>item.name}
           />
         </SafeAreaView>
       );
@@ -36,10 +48,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
       },
-     
       gridText: {
         fontSize: 15,
         color: 'black'
+      },
+      touch: {
+        height: 50,
+        width:100,
+        paddingVertical: 10
       }
   });
 
