5b72cfe1ef363ffbbe4c954f288d7591a865bf4d
dimplehh <dimplehh@naver.com>
2022-04-17 15:33:33 +0900

    크롤링 보이기(제품 사진까지)

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar ,ActivityIndicator} from 'react-native';
+import { Image, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar ,ActivityIndicator} from 'react-native';
 import {apoc} from '../../../clothes_information.json';
 
-const Item = ({ name }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{name}</Text>
-  </View>
-);
+const GridView = ({ name, image, price}) => (
+    <View style={styles.gridbox}>
+    <Image source={{uri:image}} style={{width: 150, height: 150}}/>
+      <Text style={styles.gridText}>{name}</Text>
+      <Text style={styles.gridText}>{price}</Text>
+    </View>
+  );
+
 const Index = () => {
-    const renderItem = ({ item }) => (
-        <Item name={item.name} />
-      );
-    
       return (
         <SafeAreaView style={styles.container}>
-          <FlatList
+          <FlatList numColumns={2}
             data={apoc["001"]}
-            renderItem={renderItem}
+            renderItem={({item})=><GridView name={item.name} image={item.main_image} price={item.price}/>}
             keyExtractor={item => item.name}
+            key={item=>item.name}
           />
         </SafeAreaView>
       );
@@ -28,15 +28,19 @@ const styles = StyleSheet.create({
       flex: 1,
       marginTop: StatusBar.currentHeight || 0,
     },
-    item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-    title: {
-      fontSize: 32,
-    },
+    gridbox: {
+        flex: 1,
+        height: 230,
+        margin: 2,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+     
+      gridText: {
+        fontSize: 15,
+        color: 'black'
+      }
   });
 
 export default Index;
